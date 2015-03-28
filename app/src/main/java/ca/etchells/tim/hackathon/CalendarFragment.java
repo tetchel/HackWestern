61317f510ca2e51c261db67a37f637d4c486a6c7
wungsten <tetchel@gmail.com>
2015-03-28 13:32:47 -0400

    Tabbed layout now works with calendar

--- app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
+++ app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
@@ -1,21 +1,26 @@
 package ca.etchells.tim.hackathon;
 
-import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.Toast;
+
 import com.example.tim.hackathon.R;
 
-public class CalendarFragment extends Activity {
+public class CalendarFragment extends Fragment {
     CalendarView calendar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calendar_fragment);
-        calendar = (CalendarView) findViewById(R.id.calendarView1);
+        View root = inflater.inflate(R.layout.calendar_fragment, container, false);
+
+        calendar = (CalendarView) root.findViewById(R.id.calendarView1);
         calendar.setOnDateChangeListener(new OnDateChangeListener() {
 
             @Override
@@ -23,11 +28,12 @@ public class CalendarFragment extends Activity {
                                             int month, int dayOfMonth) {
                 // TODO Auto-generated method stub
                 Toast.makeText(
-                        getApplicationContext(),
+                        getActivity(),
                         "Selected date is :\n\n" + dayOfMonth + "-" + month
-                                + "-" + year, Toast.LENGTH_LONG).show();
+                                + "-" + year, Toast.LENGTH_SHORT).show();
             }
         });
-    }
 
+        return root;
+    }
 }
