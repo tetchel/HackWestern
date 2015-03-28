066fc343b8e0497ab780ed9c4d7488eadc70d254
wungsten <tetchel@gmail.com>
2015-03-28 08:55:45 -0400

    Calendar activity layout works

--- app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
+++ app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
@@ -5,14 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import com.example.tim.hackathon.R;
 
 public class CalendarFragment extends Fragment {
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.calendar_fragment, container, false);
+
+        CalendarView calendarView = (CalendarView) rootView.findViewById(R.id.calendarView);
 
         return rootView;
     }
