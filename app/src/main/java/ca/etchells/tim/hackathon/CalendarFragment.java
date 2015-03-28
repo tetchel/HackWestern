5aab620424c4f3909280e1ba6a17a1d0f37282ac
wungsten <tetchel@gmail.com>
2015-03-28 08:48:55 -0400

    Added calendar layout

--- app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
+++ app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
@@ -1,33 +1,18 @@
 package ca.etchells.tim.hackathon;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tim.hackathon.R;
-import com.roomorama.caldroid.CaldroidFragment;
-
-import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.id.calendar_fragment, container, false);
-
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
-        Bundle args = new Bundle();
-        Calendar cal = Calendar.getInstance();
-        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-        caldroidFragment.setArguments(args);
-
-        FragmentTransaction t = getFragmentManager().beginTransaction();
-        t.replace(R.id.calendar, caldroidFragment);
-        t.commit();
+        View rootView = inflater.inflate(R.layout.calendar_fragment, container, false);
 
         return rootView;
     }
