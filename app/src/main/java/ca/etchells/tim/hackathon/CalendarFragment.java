9c9ae0708a0723ad8c49fa078f5db8f8549b03fa
karenzeng <kzeng4@uwo.ca>
2015-03-28 12:02:56 -0400

    attempt at a calendar, with working function

--- app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
+++ app/src/main/java/ca/etchells/tim/hackathon/CalendarFragment.java
@@ -1,21 +1,33 @@
 package ca.etchells.tim.hackathon;
 
-import android.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.CalendarView;
-
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 import com.example.tim.hackathon.R;
 
-public class CalendarFragment extends Fragment {
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.calendar_fragment, container, false);
+public class CalendarFragment extends Activity {
+    CalendarView calendar;
 
-        CalendarView calendarView = (CalendarView) rootView.findViewById(R.id.calendarView);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_fragment);
+        calendar = (CalendarView) findViewById(R.id.calendarView1);
+        calendar.setOnDateChangeListener(new OnDateChangeListener() {
 
-        return rootView;
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Selected date is :\n\n" + dayOfMonth + "-" + month
+                                + "-" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
