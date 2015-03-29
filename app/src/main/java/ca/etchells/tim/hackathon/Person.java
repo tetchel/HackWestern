533b81e3c97021830571eb8a809f799eb0bb7b20
Rohit <rohit.rd.das@gmail.com>
2015-03-28 20:28:25 -0400

    Created single_person.xml and person.java

---
+++ app/src/main/java/ca/etchells/tim/hackathon/Person.java
@@ -0,0 +1,16 @@
+package ca.etchells.tim.hackathon;
+
+import android.media.Image;
+
+/**
+ * Created by Rohit on 15-03-28.
+ */
+public class Person {
+
+    private int phoneNumber;
+    private String firstName;
+    private String lastName;
+    private Image picture;
+
+
+}
